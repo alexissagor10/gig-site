683f679ae8940356db974fe0d9642fde8fa0148c
alexissagor10 <78777431+alexissagor10@users.noreply.github.com>
2021-02-09 12:53:25 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function(){
+	jQuery('.owl-carousel').owlCarousel({
+	    margin:5,
+	    loop:true,
+	    autoplay: true,
+	    autoplayTimeout: 2000,
+	    items:3
+	});
+	jQuery(".menu-bar").click(function(){
+		jQuery(".news-feed-menu").slideToggle(1500);
+	});
+	jQuery(window).resize(function(){
+		var screenWidth= jQuery(window).width();
+		if (screenWidth > 768){
+			jQuery(".news-feed-menu").removeAttr("style");
+		}
+	});
+	jQuery('.emojione').emojioneArea();
+	jQuery('.post-reaction .react').hover(function(){
+		jQuery('.reactions').fadeToggle(500);
+	});
+	jQuery('.post-reaction .reactions').hover(function(){
+		jQuery('.reactions').css('display','block');
+	});
+	jQuery('.menu-options').click(function(){
+		jQuery('.options').show();
+	});
+	jQuery('.menu-options').click(function(){
+		jQuery('.profile-show').toggle();
+	});
+});
